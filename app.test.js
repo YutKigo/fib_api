25adf0d90085488cad04209639310df2a665ec20
YutKigo <ykigoshi0829@gmail.com>
2025-01-04 14:11:43 +0900

    huge input test was added

--- app.test.js
+++ app.test.js
@@ -7,14 +7,14 @@ describe('fib_api', () => {
     it('GET /fib?n=1 - success', async () => {
         const response = await request(app).get('/fib').query({ n: 1 });
         expect(response.statusCode).toBe(200);
-        expect(response.body).toEqual({ result: 1 });
+        expect(response.body).toEqual({ result: "1" });
     });
 
     // 正常な入力のテスト（n = 10）
     it('GET /fib?n=10 - success', async () => {
         const response = await request(app).get('/fib').query({ n: 10 });
         expect(response.statusCode).toBe(200);
-        expect(response.body).toEqual({ result: 55 });
+        expect(response.body).toEqual({ result: "55" });
     });
 
     // 0入力のテスト（n = 0）
@@ -29,5 +29,12 @@ describe('fib_api', () => {
         const response = await request(app).get('/fib').query({ n: "abc" });
         expect(response.statusCode).toBe(400);
         expect(response.body).toEqual({ status: 400, message: 'Bad request.' });
+    });
+
+    // 大きな入力のテスト（n = 1000）
+    it('GET huge input: 1000', async () => {
+        const response = await request(app).get('/fib').query({ n: 1000 });
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({ result: "43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875" });
     });
 })
